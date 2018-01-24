c1671d95c72a75783985ae29d75a0266fd06dfc7
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-24 16:38:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ var config = {
   startup: function() {var x = 0; var y = 0;},
 background: function() {ctx.fillStyle="white";canvasJS.rectangle(0,0,500,500);},
 gameClick: function(e) {},
-gameKey: function(e) {
-if (e.keyCode === 37) {
+gameKey: function() {
+if (code === 37) {
     x--;
-    } else if (e.keyCode === 38) {
+    } else if (code === 38) {
       y--;
-} else if (e.keyCode === 39) {
+} else if (code === 39) {
           x++;     
                
-           } else if (e.keyCode === 40) {
+           } else if (code === 40) {
     y++;
     }
 
