d3bd890cf1ca6972a839753b099935c4ef3a8702
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-24 16:27:24 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 var config = {
-  startup: function() {},
-background: function() {},
+  startup: function() {var x = 0; var y = 0;},
+background: function() {ctx.fillStyle="white";canvasJS.rectangle(0,0,500,500);},
 gameClick: function(e) {},
-gameKey: function(e) {},
+gameKey: function(e) {
+
+
+},
 gameTouch: function(e) {},
 gamePlay: function() {}
 };
