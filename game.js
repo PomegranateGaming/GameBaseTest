444a9054f475eec6742f01c178061ee537f9f583
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-24 16:37:31 -0500

    Update game.js

--- game.js
+++ game.js
@@ -2,14 +2,19 @@ function clear() {
 config.background();
 }
 // set variables
+var code;
+var key = false;
 var x = 0;
 var y = 0;
 function char() {
 config.gamePlay();
+                   if(key){config.gameKey();}
 }
 var touch = 0;
         window.addEventListener('keydown', function (e) {
-            config.gameKey(e);
+                code = e.keyCode;
+                key = true;
+ 
         });
 window.addEventListener('touchstart', function () {
              if (y===400) {touch = 1;
@@ -27,7 +32,11 @@ window.addEventListener('click', function (e) {
             config.gameClick(e);
 });
 
-
+window.addEventListener('keyup', function () {
+                
+                key = false;
+ 
+        });
 
 setInterval(function(){clear();char();},10);
 
